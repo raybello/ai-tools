462be8563f69959494e33f707152a2409f218966
RaymondBello <Raymondbello11@gmail.com>
2025-06-11 16:06:37 -0400

    patches

--- browser-use/main.py
+++ browser-use/main.py
@@ -1,16 +1,27 @@
 from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
 from browser_use import Agent
 from dotenv import load_dotenv
 load_dotenv()
 
 import asyncio
 
-llm = ChatOpenAI(model="gpt-3")
+# llm = ChatOpenAI(model="gpt-3")
+# llm = ChatOllama(model="qwen3:4b")
+llm = ChatOllama(model="qwen3:8b")
+# llm = ChatOllama(model="devstral:latest")
+# llm = ChatOllama(model="deepseek-r1:8b")
+
+
+initial_actions = [
+	{'open_tab': {'url': 'https://finance.yahoo.com/quote/TSLA/'}},
+]
 
 async def main():
     agent = Agent(
         task="What is Tesla(TSLA) latest stock price",
         llm=llm,
+        initial_actions=initial_actions
     )
     result = await agent.run()
     print(result)
--- hunter-ai/realtorca-scrape.py
+++ hunter-ai/realtorca-scrape.py
@@ -1,19 +1,35 @@
 import asyncio
 from crawl4ai import AsyncWebCrawler, BrowserConfig, CrawlerRunConfig, CacheMode
-
+from bs4 import MarkupResemblesLocatorWarning, BeautifulSoup
+import warnings
+    
 async def main():
+    
+    filehandle = open("C:/Users/raymo/Github/ai-tools/hunter-ai/assets/page1.html")
+    
+    soup = BeautifulSoup(filehandle)
+    # print(soup)
 
+    local_file_path = "C:/Users/raymo/Github/ai-tools/hunter-ai/assets/page2.htm"  # Replace with your file path
+    # file_url = f"file://{local_file_path}"
+    file_url = f"raw:{soup}"
+    config = CrawlerRunConfig(cache_mode=CacheMode.BYPASS)
+    
     async with AsyncWebCrawler() as crawler:
-        result = await crawler.arun(url="file://assets/page1.html")
-        print(result)
+        result = await crawler.arun(url=file_url, config=config)
+        if result.success:
+            print("Markdown Content from Local File:")
+            print(result.markdown)
+        else:
+            print(f"Failed to crawl local file: {result.error_message}")
 
-        # Using configuration:
-        browser_config = BrowserConfig(browser_type="chromium", headless=True)
+        # # Using configuration:
+        browser_config = BrowserConfig(browser_type="chromium", headless=False)
         async with AsyncWebCrawler(config=browser_config) as crawler:
             crawler_config = CrawlerRunConfig(
                 cache_mode=CacheMode.BYPASS
             )
-            result = await crawler.arun(url="file://assets/page1.html", config=crawler_config)
+            result = await crawler.arun(url=file_url, config=crawler_config)
             print(result.markdown)
             
 
